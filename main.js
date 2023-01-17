cb7b60a780fab765dc4d0b65daf3f77c699bd378
Ömer Faruk <sahan.faruk.omer@outlook.com>
2023-01-17 23:52:53 +0300

    some stupid bugs

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const slider = new A11YSlider(document.querySelector(".slider"), {
   adaptiveHeight: false,
   dots: true,
   centerMode: true,
-  arrows: false,
+  arrows: true,
   responsive: {
     480: {
       dots: false, // dots enabled 1280px and up
